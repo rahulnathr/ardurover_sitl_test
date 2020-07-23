99d6f61b325de6bcaccdddbce9b06d647e2e8aa8
Rahul Nath <rahulnath2473@gmail.com>
2020-07-23 10:57:43 +0200

    Feat:Add ros handler for publishing pose

--- src/rover_rc_controller.py
+++ src/rover_rc_controller.py
@@ -1,33 +1,56 @@
+#!/usr/bin/env python
 from rover_rc import View
 from rover_model import Model
-import tkinter as tk
-class Controller:
+from rover_model import LocationMavros
+import Tkinter as tk
+import rospy 
+class Controller(object):
     def __init__(self):
         self.view = View(self)
         self.model = Model()
+        self.location = LocationMavros()
         self.x1 = 0
         self.y1 = 0
 
-    def sent_coords2(self):
-        self.conversion_to_integers()
-        self.update_mission()
+    def sent_coordinates_two(self):
+        self.conversion_to_integers_two()
+        self.update_mission_two()
         coords = [self.model.missionx2,self.model.missiony2,
                   self.model.missionx2+5,self.model.missiony2+5]
         self.view.canvas_plot.create_rectangle(coords,fill ='red')
 
-    def conversion_to_integers(self):
-        self.x1 = float(self.view.entry11x.get())
-        self.y1 = float(self.view.entry22y.get())
+    def conversion_to_integers_one(self):
+        self.x1 = float(self.view.entry1x.get())
+        self.y1 = float(self.view.entry1y.get())
+        self.model.x = self.x1
+        self.model.y = self.y1
+    
+    def sent_coordinates_one(self):
+        self.conversion_to_integers_one()
+        self.update_mission_one()
+        coords = [self.model.missionx1,self.model.missiony1,
+                  self.model.missionx1+5,self.model.missiony1+5]
+        self.view.canvas_plot.create_rectangle(coords,fill ='red')
+    
+    def update_mission_one(self):
+        self.model.missionx1 = self.model.originx+self.model.x
+        self.model.missiony1 = self.model.originy+self.model.y
+
+
+    def conversion_to_integers_two(self):
+        self.x1 = float(self.view.entry2x.get())
+        self.y1 = float(self.view.entry2y.get())
         self.model.x = self.x1
         self.model.y = self.y1
 
-    def update_mission(self):
+    def update_mission_two(self):
         self.model.missionx2 = self.model.originx + self.model.x
         self.model.missiony2 = self.model.originy + self.model.y
 
 
 
-
 if __name__ == "__main__":
+    rospy.init_node("location_node")
     app = Controller()
+    
     app.view.mainloop()
