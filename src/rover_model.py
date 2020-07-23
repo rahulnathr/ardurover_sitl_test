ed9e7c2f27c2535dc2c28e4173f5a824a2293c1a
Rahul Nath <rahulnath2473@gmail.com>
2020-07-23 10:56:21 +0200

    Feat:add subscriber for global/local

--- src/rover_model.py
+++ src/rover_model.py
@@ -1,8 +1,14 @@
-import tkinter as tk
+#!/usr/bin/env python
+import Tkinter as tk
+import rospy
+from nav_msgs.msg import Odometry
 
-class Model:
+
+
+class Model(object):
     def __init__(self):
-        self.originx = 175
+        # self.controller = controller
+        self.originx = 200
         self.originy = 250
         self.x = 0
         self.y = 0
@@ -10,3 +16,19 @@ class Model:
         self.missiony1 = self.originy+self.y
         self.missionx2 = self.originx+self.y
         self.missiony2 = self.originy+self.y
+
+class LocationMavros(object):
+    def __init__(self):
+        self.locationSubscriber = rospy.Subscriber("/mavros/global_position/local",Odometry,self.print_location)
+        #format is (topic name,topic type,callback)
+    
+
+    def print_location(self,msg):
+        x = msg.pose.pose.position.x
+        y = msg.pose.pose.position.y
+        rospy.loginfo("X value %f",x)
+        rospy.loginfo("Y value %f",y)
+
+
+
+    
